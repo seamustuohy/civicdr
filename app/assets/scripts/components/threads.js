360a79b03069e6eaf3609a5a617171a5f8adb61c
seamus tuohy <code@seamustuohy.com>
2017-08-16 09:56:46 -0400

    moved check to after newMessage object exists.

--- app/assets/scripts/components/threads.js
+++ app/assets/scripts/components/threads.js
@@ -19,6 +19,7 @@ class Threads extends React.Component {
     }
   }
 
+
   render () {
     // These will not exist for users who aren't authorized to view them
     const note = this.props.threads.find(t => t.type === 'note');
@@ -72,10 +73,8 @@ class Threads extends React.Component {
              disabled={this.newMessageField.value === ''}
              className='button button--large button--base'
              onClick={() => {
-               if (this.newMessageField.value !== '') {
                  this.props.create(visibleThread.id, this.newMessageField.value);
                  this.newMessageField.value = '';
-               }
              }
                      }>Comment</button>
             </div>
