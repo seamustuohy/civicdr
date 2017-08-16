df94e7ca8ae42b9e20a9d9c9caf9574778f9f099
seamus tuohy <code@seamustuohy.com>
2017-08-16 13:44:03 -0400

    added routermiddleware def

--- app/assets/scripts/utils/store.js
+++ app/assets/scripts/utils/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import {browserHistory} from 'react-router';
+import {routerMiddleware} from 'react-router-redux';
 
 import config from '../config';
 import reducer from '../reducers';
